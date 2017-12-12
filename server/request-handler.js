cc80d96828e6df0b4589af18f7d96e1e1a22bd19
Andrew Lichtenstein <andrew@Andrews-MacBook-Pro.local>
2017-12-12 10:35:07 -0500

    completed basic functionality

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,49 +12,6 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
-
-  // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
-
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end('Hello, World!');
-};
-
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
 // are on different domains, for instance, your chat client.
@@ -71,3 +28,41 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
+// Results array with message objects
+var results = [{'username': 'Hackerman', 'text': 'This is a test message', 'roomname': 'Generic Room', 'createdAt': new Date()}];
+
+var requestHandler = function(request, response) {
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+  
+  // The outgoing status.
+  var statusCode = 200;
+  
+  // Check endpoint and method 
+  if (request.url.slice(0, 16) === '/classes/message') {
+    if (request.method === 'GET') {
+
+    } else if (request.method === 'POST') {
+      statusCode = 201;
+      var message;
+      request.on('data', (data) => {
+        message = JSON.parse(data);
+        if (!message['roomname']) {
+          message['roomname'] = undefined;
+        }
+        message['createdAt'] = new Date();
+        results.push(message);
+      });
+    }
+  } else {
+    statusCode = 404;
+  }
+
+  // Set headers
+  var headers = defaultCorsHeaders;
+  headers['Content-Type'] = 'application/json';
+  response.writeHead(statusCode, headers);
+
+  response.end(JSON.stringify({ 'results': results.reverse() }));
+};
+
+exports.requestHandler = requestHandler;
